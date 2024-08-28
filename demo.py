336e1d5175b798052fb2cb91d448addee2849935
jshep23prog <jshep23@gmail.com>
2024-08-28 15:53:19 -0500

    init the game window

---
+++ demo.py
@@ -0,0 +1,18 @@
+import pygame
+
+#init pygame instance
+pygame.init()
+
+#setting up the game window
+
+window_width = 800
+window_height = 600
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Snake Game")
+
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
