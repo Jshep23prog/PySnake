887d282c9bdd865194eab6d9ff88692d64a272ca
jshep23prog <jshep23@gmail.com>
2024-08-28 16:01:05 -0500

    drawing snake on window

--- demo.py
+++ demo.py
@@ -10,9 +10,16 @@ window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Snake Game")
 
+green = (0, 128, 0)
 game_over = False
 
+
+#game loop logic
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+    #write logic outside of the for loop
+    pygame.draw.rect(window, green, [400, 300, 10, 10]) #400 and 300 are where the snake begins in centerpoint. 10, 10 is the size of pixel
+    #need to call update for snake to show on window
+    pygame.display.update()
